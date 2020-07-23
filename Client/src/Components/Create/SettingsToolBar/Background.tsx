c9311889768a56fa88c31a48d7dcf0ad3406d916
yovel <yovel78910@gmail.com>
2020-07-23 20:07:47 +0300

    test

--- Client/src/Components/Create/SettingsToolBar/Background.tsx
+++ Client/src/Components/Create/SettingsToolBar/Background.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { BackgroundType } from "../../../types";
+import { BackgroundType } from "../../../Types";
 
 const Background: React.FC = () => {
   const dispatch = useDispatch();
