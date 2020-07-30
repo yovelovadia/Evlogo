c750c1ed90bfc3aced9a4d621f16cb4544bf9e5f
yovel <yovel78910@gmail.com>
2020-07-30 09:23:55 +0300

    change

--- Client/src/Components/Create/SettingsToolBar/StickersAndImages/Carousels/ImageInsideCarousel.tsx
+++ Client/src/Components/Create/SettingsToolBar/StickersAndImages/Carousels/ImageInsideCarousel.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const ImageInCarousel: React.FC<Props> = (props) => {
   const dispatch: any = useDispatch();
-  const [src, setSrc] = useState<string>(`http://localhost:5000/${props.src}`);
+  const [src, setSrc] = useState<string>(`https://evlogo.herokuapp.com/${props.src}`);
   let checkErrorHeppend = useRef<boolean>(false);
 
   return (
@@ -21,7 +21,7 @@ const ImageInCarousel: React.FC<Props> = (props) => {
         alt={"#"}
         onError={() => {
           if (!checkErrorHeppend.current) {
-            setSrc(src.slice(22));
+            setSrc(src.slice(28));
           }
           checkErrorHeppend.current = true;
         }}
